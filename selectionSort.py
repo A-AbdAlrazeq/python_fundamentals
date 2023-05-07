2ee36ce4e40f2fbb3456dd7aa40730b5ac9c3a7a
A-AbdAlrazeq <107461563+A-AbdAlrazeq@users.noreply.github.com>
2023-05-07 17:33:21 +0300

    Insertion Sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,11 +1,14 @@
 def selectionSort(list):
     for i in range(0, len(list)-1):
+        print("i", i)
         min = i
         for j in range(i+1, len(list)):
             if list[j] < list[min]:
                 min = j
         if min != i:
+            print("min", min)
             list[i], list[min] = list[min], list[i]
+            print("list", "*"*50, list)
     return list
 
 
